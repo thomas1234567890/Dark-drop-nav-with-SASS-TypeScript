d50461982ea6f9974d97ee9a3b932a9799f46930
Thomas1234567890 <tompatcon@gmail.com>
2021-01-15 02:57:05 +0000

    3 files modified

--- index.ts
+++ index.ts
@@ -3,16 +3,18 @@ import "./styles/style.scss";
 
 console.clear()
 
+
+
 /*make text visible on card hover*/
 
-const on_card_mouse_enter =() =>
-{
-  document.getElementById('card_discription').style.opacity='1';
-}
-const on_card_mouse_leave =() =>
-{
-  document.getElementById('card_discription').style.opacity='0';
-} 
+// const on_card_mouse_enter =() =>
+// {
+//   document.getElementById('card_discription').style.opacity='1';
+// }
+// const on_card_mouse_leave =() =>
+// {
+//   document.getElementById('card_discription').style.opacity='0';
+// } 
 
 // const el = document.getElementById("card_1");
 // el.addEventListener("mouseenter", on_card_mouse_enter);
@@ -21,21 +23,21 @@ const on_card_mouse_leave =() =>
 
 
 
-document.querySelectorAll('.card').forEach(item => {
-  item.addEventListener('mouseenter', function(e)
-  {
-    // console.log(e.target.id);
-    // let x = document.getElementById(e.target.id)
-    // let y = x.getElementsByClassName('card_discription');
-    // console.log(y[0]);
-    // y[0].innerHTML
+// document.querySelectorAll('.card').forEach(item => {
+//   item.addEventListener('mouseenter', function(e)
+//   {
+//     // console.log(e.target.id);
+//     // let x = document.getElementById(e.target.id)
+//     // let y = x.getElementsByClassName('card_discription');
+//     // console.log(y[0]);
+//     // y[0].innerHTML
 
-    // let x = document.getElementById(e.target.id);
-    // let y = x.getElementsByClassName('.card_discription');
-    // y.style.opacity='1';
+//     // let x = document.getElementById(e.target.id);
+//     // let y = x.getElementsByClassName('.card_discription');
+//     // y.style.opacity='1';
     
-  });
+//   });
 
 
-  // item.addEventListener("mouseleave", on_card_mouse_leave);
-})
+//   // item.addEventListener("mouseleave", on_card_mouse_leave);
+// })

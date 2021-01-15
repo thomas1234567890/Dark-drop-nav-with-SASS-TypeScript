77005b6d24812c6e0c7ac240fb14ffe52f97499a
Thomas1234567890 <tompatcon@gmail.com>
2021-01-15 02:11:39 +0000

    4 files modified, 3 added

--- index.ts
+++ index.ts
@@ -1,2 +1,41 @@
 // Import stylesheets
 import "./styles/style.scss";
+
+console.clear()
+
+/*make text visible on card hover*/
+
+const on_card_mouse_enter =() =>
+{
+  document.getElementById('card_discription').style.opacity='1';
+}
+const on_card_mouse_leave =() =>
+{
+  document.getElementById('card_discription').style.opacity='0';
+} 
+
+// const el = document.getElementById("card_1");
+// el.addEventListener("mouseenter", on_card_mouse_enter);
+// el.addEventListener("mouseleave", on_card_mouse_leave);
+
+
+
+
+document.querySelectorAll('.card').forEach(item => {
+  item.addEventListener('mouseenter', function(e)
+  {
+    // console.log(e.target.id);
+    // let x = document.getElementById(e.target.id)
+    // let y = x.getElementsByClassName('card_discription');
+    // console.log(y[0]);
+    // y[0].innerHTML
+
+    // let x = document.getElementById(e.target.id);
+    // let y = x.getElementsByClassName('.card_discription');
+    // y.style.opacity='1';
+    
+  });
+
+
+  // item.addEventListener("mouseleave", on_card_mouse_leave);
+})
